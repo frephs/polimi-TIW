a18d5160fed86e702451508f435e636b3420843e
frephs <ffrancescogenovese@gmail.com>
2025-04-19 16:54:56 +0200

    feat: update account manager controller

--- app/auction-app/src/main/java/it/polimi/auctionapp/controllers/AccountManager.java
+++ app/auction-app/src/main/java/it/polimi/auctionapp/controllers/AccountManager.java
@@ -24,17 +24,10 @@ public class AccountManager {
 
         @Override
         public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-            if(request.getSession().isNew()){
-                request.getSession().setAttribute("user", null);
-                // FIXME: This causes first-click bug
-                response.sendRedirect(getServletContext().getContextPath() + "/");
-            }
-
             if (request.getSession().getAttribute("user") == null) {
-                processTemplate("/account", request, response);
+                processTemplate(request, response, "/account");
             } else {
-                processTemplate("/account", request, response);
-
+                processTemplate(request, response, "/account-details");
             }
 
         }
@@ -45,35 +38,30 @@ public class AccountManager {
     public static class LoginManagerServlet extends ThymeleafHTTPServlet {
         @Override
         public void doPost(HttpServletRequest request, HttpServletResponse response) {
-            HttpSession session = request.getSession();
             String username = request.getParameter("username");
             String password = request.getParameter("password");
-
             try {
                 User user = userConnectionHandler.getUser(username, password);
                 request.getSession().setAttribute("user", user);
                 request.getSession().setAttribute("message", MessageType.SUCCESS.wrap("User logged in successfully."));
-            } catch(SQLWarning e) {
-                request.setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
+            } catch (SQLWarning e) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
             } catch (SQLException e) {
-                request.setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+                request.getSession().setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+            } finally {
+                sendRedirect(request, response, "/account");
             }
-
         }
     }
 
-    @WebServlet("/logout")
+    @WebServlet("/account/logout")
     public static class LogoutManagerServlet extends ThymeleafHTTPServlet {
         @Override
         public void doPost(HttpServletRequest request, HttpServletResponse response) {
-            HttpSession session = request.getSession();
-            session.invalidate();
-            try {
-                session.setAttribute("message", MessageType.SUCCESS.wrap("User logged out successfully."));
-                response.sendRedirect("/");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            request.getSession().invalidate();
+            request.getSession().setAttribute("user", null);
+            request.getSession().setAttribute("message", MessageType.SUCCESS.wrap("User logged out successfully."));
+            sendRedirect(request, response, "/");
         }
     }
 
@@ -98,18 +86,13 @@ public class AccountManager {
                         request.getParameter("password")
                 );
                 session.setAttribute("message", MessageType.SUCCESS.wrap("User registered successfully. Now log in with your credentials"));
-                processTemplate("/account", request, response);
-
-            }catch (SQLWarning e){
+            } catch (SQLWarning e) {
                 session.setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
-                processTemplate("/account", request, response);
             } catch (SQLException e) {
                 session.setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
-                processTemplate("/account", request, response);
-            }finally {
-                session.removeAttribute("message");
+            } finally {
+                sendRedirect(request, response, "/account");
             }
-
         }
     }
 
@@ -117,73 +100,141 @@ public class AccountManager {
     public static class DeleteAccountManagerServlet extends ThymeleafHTTPServlet {
         @Override
         public void doPost(HttpServletRequest request, HttpServletResponse response) {
+            try {
+                userConnectionHandler.deleteAccount(
+                        ((User) request.getSession().getAttribute("user")).getUsername(),
+                        request.getParameter("password")
+                );
+                request.getSession().setAttribute("user", null);
+                request.getSession().invalidate();
+                request.getSession().setAttribute("message", MessageType.SUCCESS.wrap("Account deleted successfully"));
+                sendRedirect(request, response, "/");
+            } catch (SQLWarning e) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
+                sendRedirect(request, response, "/account");
+            } catch (SQLException e) {
+                request.getSession().setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+                sendRedirect(request, response, "/account");
 
+            }
         }
     }
 
 
-    @WebServlet("/account/updateUsername")
+    @WebServlet("/account/update-username")
     public static class UpdateUsernameServlet extends ThymeleafHTTPServlet {
         @Override
         public void doPost(HttpServletRequest request, HttpServletResponse response) {
-            HttpSession session = request.getSession();
-            try {
-                userConnectionHandler.updateUsername(
-                        ((User) request.getSession().getAttribute("user")).getUsername(),
-                        request.getParameter("newUsername"),
-                        request.getParameter("password")
-                );
-            } catch (SQLWarning e) {
-                request.setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
-            } catch (SQLException e) {
-                request.setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+            if (request.getParameter("new-username").equals(
+                    ((User) request.getSession().getAttribute("user")).getUsername()
+            )) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap("New username must be different from the old one"));
+            } else if (request.getParameter("new-username").isEmpty()) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap("New username cannot be empty"));
+            } else if (request.getParameter("password").isEmpty()) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap("Password cannot be empty"));
+            } else {
+                try {
+                    userConnectionHandler.updateUsername(
+                            ((User) request.getSession().getAttribute("user")).getUsername(),
+                            request.getParameter("new-username"),
+                            request.getParameter("password")
+                    );
+                    request.getSession().setAttribute("user",
+                            userConnectionHandler.getUser(
+                                    request.getParameter("new-username"),
+                                    request.getParameter("password")
+                            )
+                    );
+                    request.getSession().setAttribute("message", MessageType.SUCCESS.wrap("Username updated successfully"));
+                } catch (SQLWarning e) {
+                    request.getSession().setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
+                } catch (SQLException e) {
+                    request.getSession().setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+                }
             }
+            sendRedirect(request, response, "/account");
         }
     }
 
-    @WebServlet("/account/updatePassword")
+
+    @WebServlet("/account/update-password")
     public static class UpdatePasswordServlet extends ThymeleafHTTPServlet {
         @Override
         public void doPost(HttpServletRequest request, HttpServletResponse response) {
-            HttpSession session = request.getSession();
-            try {
-                userConnectionHandler.updatePassword(((User) request.getSession().getAttribute("user")).getUsername(),
-                        request.getParameter("newPassword"),
-                        request.getParameter("oldPassword")
-                );
-            } catch (SQLWarning e) {
-                request.setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
-            } catch (SQLException e) {
-                request.setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+            if (!request.getParameter("new-password").equals(request.getParameter("confirm-new-password"))) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap("New password and confirmation do not match"));
+            } else if (
+                    request.getParameter("new-password").isEmpty() ||
+                            request.getParameter("confirm-new-password").isEmpty() ||
+                            request.getParameter("password").isEmpty()
+            ) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap("Password cannot be empty"));
+            } else {
+                try {
+                    userConnectionHandler.updatePassword(((User) request.getSession().getAttribute("user")).getUsername(),
+                            request.getParameter("password"),
+                            request.getParameter("new-password")
+                    );
+                    request.getSession().setAttribute("message", MessageType.SUCCESS.wrap("Password updated successfully"));
+                } catch (SQLWarning e) {
+                    request.getSession().setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
+                } catch (SQLException e) {
+                    request.getSession().setAttribute("message", MessageType.ERROR.wrap(e.getMessage()));
+                }
             }
+            sendRedirect(request, response, "/account");
         }
     }
 
-    @WebServlet("/account/updateAccountDetails")
+    @WebServlet("/account/update-details")
     public static class UpdateManagerServlet extends ThymeleafHTTPServlet {
         @Override
         public void doPost(HttpServletRequest request, HttpServletResponse response) {
-            HttpSession session = request.getSession();
-            try {
-                userConnectionHandler.updateAccountDetails(
-                        new User(
+            if (
+                    request.getParameter("name").isEmpty() ||
+                            request.getParameter("surname").isEmpty() ||
+                            request.getParameter("country").isEmpty() ||
+                            request.getParameter("zip-code").isEmpty() ||
+                            request.getParameter("city").isEmpty() ||
+                            request.getParameter("street").isEmpty() ||
+                            request.getParameter("street-number").isEmpty()
+            ) {
+                request.getSession().setAttribute("message", MessageType.WARNING.wrap("All fields must be filled"));
+            } else {
+                try {
+                    if (Integer.parseInt(request.getParameter("street-number")) < 1) {
+                        request.getSession().setAttribute("message", MessageType.WARNING.wrap("Street number must be a positive number"));
+                    } else if (Integer.parseInt(request.getParameter("zip-code")) < 10000 || Integer.parseInt(request.getParameter("zip-code")) > 99999) {
+                        request.getSession().setAttribute("message", MessageType.WARNING.wrap("Zip code must be a positive 5 digit number "));
+                    } else {
+                        User new_user = new User(
                                 ((User) request.getSession().getAttribute("user")).getUsername(),
                                 request.getParameter("name"),
                                 request.getParameter("surname"),
                                 new Address(
                                         request.getParameter("country"),
-                                        Integer.parseInt(request.getParameter("zipCode")),
+                                        Integer.parseInt(request.getParameter("zip-code")),
                                         request.getParameter("city"),
                                         request.getParameter("street"),
-                                        Integer.parseInt(request.getParameter("street_number"))
+                                        Integer.parseInt(request.getParameter("street-number"))
                                 )
-                        )
-                );
-            } catch (SQLException e) {
-                request.setAttribute("message", MessageType.WARNING.wrap(e.getMessage()));
+                        );
+                        userConnectionHandler.updateAccountDetails(
+                                new_user
+                        );
+                        request.getSession().setAttribute("message", MessageType.SUCCESS.wrap("Account details updated successfully"));
+                        request.getSession().setAttribute("user", new_user);
+                    }
+                } catch (SQLException e) {
+                    request.getSession().setAttribute("message", "Something went wrong while updating your details. " + MessageType.ERROR.wrap(e.getMessage()));
+                } catch (NumberFormatException e) {
+                    request.getSession().setAttribute("message", MessageType.WARNING.wrap(
+                            "Street number and zip code must be numbers"
+                    ));
+                }
             }
+            sendRedirect(request, response, "/account");
         }
-
-
     }
 }
